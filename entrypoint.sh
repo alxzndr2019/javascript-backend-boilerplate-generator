0dad50302f5bf6f48f0370e040f6d50a87753adc
Alex Ohioze Omiunu <45854856+alxzndr2019@users.noreply.github.com>
2024-01-08 18:23:24 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/index.js
+node /usr/bin/index.js
 
 git add -A && commit -m "Generate boilerplate"
 
