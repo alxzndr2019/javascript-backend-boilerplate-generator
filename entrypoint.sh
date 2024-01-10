5993b7a1c1e7b4edc88a543ff723d086220432a9
Alex Ohioze Omiunu <45854856+alxzndr2019@users.noreply.github.com>
2024-01-10 23:49:34 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,10 @@ git config --global --add safe.directory /github/workspace/
 # Move to the GitHub workspace (where the repository is mounted)
 cd /github/workspace/
 
-# Run your script to generate the project structure
-node /app/index.js
-
-# Add some content to the files
-find . -type f -exec sh -c 'echo "Initial content" > "{}"' \;
+# Create a text file with "Hello, World!"
+echo "Hello, World!" > hello.txt
 
 # Commit and push changes
-git add -A && git commit -m "Generate boilerplate" && git push
+git add -A && git commit -m "Add hello.txt with Hello, World!" && git push
 
 echo "========================"
