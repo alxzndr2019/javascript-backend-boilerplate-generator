61c972b703e14281dba6c53447d8b4c02f8fffcb
Alex Ohioze Omiunu <45854856+alxzndr2019@users.noreply.github.com>
2024-01-10 00:47:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-
 const projectStructure = {
   src: ['controllers', 'models', 'routes'],
   config: ['database.js', 'env.js'],
@@ -13,22 +12,27 @@ const projectStructure = {
 function createDirectories(basePath, structure) {
   Object.keys(structure).forEach((folder) => {
     const folderPath = path.join(basePath, folder);
-    fs.mkdirSync(folderPath);
 
-    if (Array.isArray(structure[folder])) {
-      structure[folder].forEach((file) => {
-        const filePath = path.join(folderPath, file);
-        fs.writeFileSync(filePath, '', 'utf-8'); 
-        console.log(`Created file: ${filePath}`);
-      });
+    // Check if the directory already exists
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+
+      if (Array.isArray(structure[folder])) {
+        structure[folder].forEach((file) => {
+          const filePath = path.join(folderPath, file);
+          fs.writeFileSync(filePath, '', 'utf-8');
+          console.log(`Created file: ${filePath}`);
+        });
+      } else {
+        createDirectories(folderPath, structure[folder]);
+      }
+
+      console.log(`Created directory: ${folderPath}`);
     } else {
-      createDirectories(folderPath, structure[folder]);
+      console.log(`Directory already exists: ${folderPath}`);
     }
-
-    console.log(`Created directory: ${folderPath}`);
   });
 }
-
 
 const rootDirectory = path.join(__dirname, 'MyProject');
 
