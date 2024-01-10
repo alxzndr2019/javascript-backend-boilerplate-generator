5ab13f156c439d1009635875012b2234245f562b
Alex Ohioze Omiunu <45854856+alxzndr2019@users.noreply.github.com>
2024-01-10 00:58:07 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,18 @@
 
 echo "========================"
 
+# Set Git configurations
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
+
+# Move to the GitHub workspace (where the repository is mounted)
+cd /github/workspace
 
 # Run your script to generate the project structure
 node /app/index.js
 
 # Commit and push changes
-git add -A && git commit -m "Generate boilerplate" && git push --set-upstream origin main
+git add -A && git commit -m "Generate boilerplate" && git push
 
 echo "========================"
